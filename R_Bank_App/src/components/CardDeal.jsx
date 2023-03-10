3751cb8470762aeab71ebfdd5db32ef33d642020
Masterchinedum <chinedu02k@gmail.com>
2023-03-10 15:10:59 +0100

    edit CardDeals with corrections

--- R_Bank_App/src/components/CardDeal.jsx
+++ R_Bank_App/src/components/CardDeal.jsx
@@ -1,12 +1,13 @@
 import { card } from '../assets';
 import styles, { layout } from '../style';
-import Button from ',/Button';
+import Button from './Button';
 
 const CardDeal = () => {
   return (
     <section className={layout.section}>
-      <div className='{layout.sectionInfo}'>
-        <h2 className='{styles.heading2}'>Find a better card deal <br className='sm:block hidden' /> in few easy steps.</h2>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' /> in few easy steps.</h2>
+        <p> Arcu tortor, pursus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris totor. Aliquet yktrices ac, ametau.</p>
 
       </div>
 
