9d0d54d215ada568835e4fc0e48fbdf393bd952b
Masterchinedum <chinedu02k@gmail.com>
2023-03-10 15:01:13 +0100

    Update Carddeals Heading drction

--- R_Bank_App/src/components/CardDeal.jsx
+++ R_Bank_App/src/components/CardDeal.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from ',/Button';
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className='{layout.sectionInfo}'>
+        <h2 className='{styles.heading2}'>Find a better card deal <br className='sm:block hidden' /> in few easy steps.</h2>
+
+      </div>
+
+    </section>
   )
 }
 
